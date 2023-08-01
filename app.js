cb288209cbcecd30514312ed58caf86ddd5e97aa
Ryan Gusti Nugraha <nugrahagusti29@gmail.com>
2023-08-01 22:36:29 +0700

    add feature gtranslate + tts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const { Configuration, OpenAIApi } = require("openai");
+const translate = require("translate-google");
+const googleTTS = require("google-tts-api");
 const qrcode = require("qrcode-terminal");
 // clien config
 const client = new Client({
@@ -50,8 +52,9 @@ client.initialize();
 client.on("message", async (msg) => {
   // info command
   if (msg.body === "!info") {
-    const message = `🤖---BOT COMMAND---🤖\n1. *!everyone* = to mention everyone in group.\n2. *!sticker* = to create sticker from image.\n3. *!gpt* _prompt_ = communicate with chatgpt`;
-    client.sendMessage(msg.from, message);
+    const message = `🤖---BOT COMMAND---🤖\n1. *!everyone* = to mention everyone in group.\n2. *!sticker* = to create sticker from image.\n3. *!gpt* _prompt_ = communicate with chatgpt.\n4. *!trans* *#lang* _text_ = to using translate ex : !trans #id i want to eat.\n5. *!tts* _text_ = to using text to speech.\n\n follow ig @digiding.id`;
+    const media = await MessageMedia.fromUrl(process.env.URL_LOGO);
+    client.sendMessage(msg.from, message, { media: media });
   }
   // mention everyone in group
   if (msg.body === "!everyone") {
@@ -74,8 +77,33 @@ client.on("message", async (msg) => {
     const attachmentData = await msg.downloadMedia();
     client.sendMessage(msg.from, attachmentData, { sendMediaAsSticker: true });
   }
+  // translate
+  if (msg.body.slice(0, 6) === "!trans") {
+    const lang = msg.body.slice(8, 10);
+    const message = msg.body.slice(11);
+    translate(message, { to: lang })
+      .then((res) => {
+        msg.reply(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  // text to speech
+  if (msg.body.slice(0, 4) === "!tts") {
+    const message = msg.body.slice(5);
+    const url = googleTTS.getAudioUrl(message, {
+      lang: "id",
+      slow: false,
+      host: "https://translate.google.com",
+    });
+
+    const media = await MessageMedia.fromUrl(url, { unsafeMime: true });
+    client.sendMessage(msg.from, media);
+  }
   // chatgpt
   if (msg.body.slice(0, 4) === "!gpt") {
+    // chatgpt
     const message = msg.body.slice(4);
     runCompletion(message).then((result) => msg.reply(result));
   }
